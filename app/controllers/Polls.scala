969097536287ef16dadd8f47bfe8a130e50ec656
Jan Grape <jan.grape@crisp.se>
2013-05-04 00:07:27 +0200

    Now, you can create and edit Polls

--- app/controllers/Polls.scala
+++ app/controllers/Polls.scala
@@ -1,9 +1,12 @@
 package controllers
 
 import play.api.mvc._
-import models.Poll
+import models.{User, Poll}
 import jp.t2v.lab.play2.auth.Auth
-import models.security.NormalUser
+import models.security.{Administrator, NormalUser}
+import play.api.data.Form
+import play.api.data.Forms._
+import anorm.{Pk, NotAssigned}
 
 object Polls extends Controller with Auth with AuthConfigImpl {
 
@@ -21,5 +24,74 @@ object Polls extends Controller with Auth with AuthConfigImpl {
       case e: Exception => NotFound(views.html.error(NOT_FOUND, "Kan inte hitta undersökning med id '" + uuid + "'"))
     }
   }
+
+  def createForm = authorizedAction(Administrator) { user => implicit request =>
+    implicit val loggedInUser = Option(user)
+    val consultants = User.findAll()
+    Ok(views.html.polls.edit(pollForm, consultants = consultants))
+  }
+
+  def create = authorizedAction(Administrator) { user => implicit request =>
+    implicit val loggedInUser = Option(user)
+    pollForm.bindFromRequest.fold(
+      formWithErrors => {
+        val consultants = User.findAll()
+        BadRequest(views.html.polls.edit(formWithErrors, consultants = consultants))
+      },
+      poll => {
+        Poll.create(poll)
+        Redirect(routes.Polls.list())
+      }
+    )
+  }
+
+  def updateForm(uuid: String) = authorizedAction(Administrator) { user => implicit request =>
+    implicit val loggedInUser = Option(user)
+    val pollToUpdate = Poll.findByUuid(uuid)
+    val consultants = User.findAll()
+    Ok(views.html.polls.edit(pollForm.fill(pollToUpdate), uuidToUpdate = Option(uuid), consultants = consultants))
+  }
+
+  def update(uuid: String) = authorizedAction(Administrator) { user => implicit request =>
+    implicit val loggedInUser = Option(user)
+    pollForm.bindFromRequest.fold(
+      formWithErrors => {
+        val consultants = User.findAll()
+        BadRequest(views.html.polls.edit(formWithErrors, uuidToUpdate = Option(uuid), consultants = consultants))
+      },
+      poll => {
+        Poll.update(uuid, poll)
+        Redirect(routes.Polls.show(uuid))
+      }
+    )
+  }
+
+
+  val pollForm: Form[Poll] = Form(
+    mapping(
+      "id" -> ignored(NotAssigned:Pk[Long]),
+      "uuid" -> ignored(""),
+      "customer" -> nonEmptyText(maxLength = 127),
+      "contactPerson" -> optional(text(maxLength = 127)),
+      "assignment" -> optional(text(maxLength = 127)),
+      "consultantId" -> longNumber,
+      "isOpen" -> boolean
+    )(toPoll)(fromPoll)
+  )
+
+  def toPoll(id: Pk[Long], uuid: String, customer: String, contactPerson:Option[String], assignment:Option[String], consultantId: Long, isOpen: Boolean): Poll = {
+    Poll(id = id,
+      uuid = uuid,
+      customer = customer,
+      contactPerson = contactPerson.getOrElse(""),
+      assignment = assignment.getOrElse(""),
+      consultant = User.find(consultantId),
+      isOpen = isOpen)
+  }
+
+  def fromPoll(poll: Poll) = {
+    Option((poll.id, poll.uuid, poll.customer, Option(poll.contactPerson), Option(poll.assignment), poll.consultant.id.get, poll.isOpen))
+  }
+
 }
 
--- app/models/Poll.scala
+++ app/models/Poll.scala
@@ -5,11 +5,11 @@ import play.api.Play.current
 import anorm._
 import anorm.SqlParser._
 import play.api.db.DB
-import java.util.UUID
+import org.apache.commons.lang3.RandomStringUtils
 
 case class Poll(
                  id: Pk[Long] = NotAssigned,
-                 uuid: String = UUID.randomUUID().toString,
+                 uuid: String = "",
                  customer: String,
                  contactPerson: String,
                  assignment: String,
@@ -46,22 +46,84 @@ object Poll {
   def find(id: Long): Poll = {
     DB.withTransaction {
       implicit connection =>
-        SQL("select * from polls where id={id}").on('id -> id).as(Poll.parser single)
+        SQL("SELECT * FROM polls WHERE id={id}").on('id -> id).as(Poll.parser single)
     }
   }
 
   def findByUuid(uuid: String): Poll = {
     DB.withTransaction {
       implicit connection =>
-        SQL("select * from polls where uuid={uuid}").on('uuid -> uuid).as(Poll.parser single)
+        SQL("SELECT * FROM polls WHERE uuid={uuid}").on('uuid -> uuid).as(Poll.parser single)
     }
   }
 
   def findAll(): Seq[Poll] = {
     DB.withTransaction {
       implicit connection =>
-        SQL("select * from polls p ORDER BY p.is_open, p.customer, p.assignment").as(Poll.parser *)
+        SQL("SELECT * FROM polls p ORDER BY p.is_open DESC, p.customer, p.assignment").as(Poll.parser *)
     }
   }
+
+  def create(poll: Poll): Long = {
+    DB.withTransaction {
+      implicit connection =>
+        SQL(insertQueryString).on(
+          'uuid -> RandomStringUtils.randomAlphanumeric(17),
+          'customer -> poll.customer,
+          'contactPerson -> poll.contactPerson,
+          'assignment -> poll.assignment,
+          'consultant -> poll.consultant.id.get,
+          'isOpen -> poll.isOpen
+        ).executeInsert()
+    } match {
+      case Some(primaryKey: Long) => primaryKey
+      case _ => throw new RuntimeException("Could not insert into database, no PK returned")
+    }
+  }
+
+  private val insertQueryString =
+    """
+INSERT INTO polls (
+      uuid,
+      customer,
+      contact_person,
+      assignment,
+      consultant,
+      is_open
+    )
+    values (
+      {uuid},
+      {customer},
+      {contactPerson},
+      {assignment},
+      {consultant},
+      {isOpen}
+    )
+    """
+
+  def update(uuid: String, poll: Poll) {
+    DB.withTransaction {
+      implicit connection =>
+        SQL(updateQueryString).on(
+          'uuid -> uuid,
+          'customer -> poll.customer,
+          'contactPerson -> poll.contactPerson,
+          'assignment -> poll.assignment,
+          'consultant -> poll.consultant.id.get,
+          'isOpen -> poll.isOpen
+        ).executeUpdate()
+    }
+  }
+
+  private val updateQueryString =
+    """
+UPDATE polls
+SET customer = {customer},
+    contact_person = {contactPerson},
+    assignment = {assignment},
+    consultant = {consultant},
+    is_open = {isOpen}
+WHERE uuid = {uuid}
+    """
 
 }
